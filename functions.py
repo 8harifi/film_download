d4c6f5864e546b64508aec846a734ba61bd94e1d
oliver <oliver.the.lizard@yandex.com>
2021-10-03 15:08:12 +0330

    the websites had some updates, so i had to update the script too(it wan

--- functions.py
+++ functions.py
@@ -31,30 +31,82 @@ def movie_links (url):
     })
     return res
 
+# def subtitle_search (search):
+#     data = {
+#         "action": "ajaxsearchlite_search",
+#         "aslp": f"{'+'.join(search.split())}",
+#         "options": "qtranslate_lang=0&asl_gen[]=title&asl_gen[]=content&customset[]=post"
+#     }
+#     r = requests.post(url = "https://subkade.ir/wp-admin/admin-ajax.php", data = data)
+#     return r.text
+
+
+# def find_subtitle_links (text):
+#     soup = bs(text, "html.parser")
+#     tags = soup.find_all('a', attrs = {
+#         "class": "asl_res_url"
+#     })
+#     links = []
+#     for tag in tags:
+#         links.append(tag.attrs['href'])
+#     return links
+
+# def get_subtitle_dl (url):
+#     r = requests.get(url = url)
+#     soup = bs(r.text, "html.parser")
+#     res = soup.find_all("a", attrs = {
+#         "class": "rounded-lg d-inline-block font-weight-bold sub_sgl_dl_btn"
+#     })
+#     dl_link = res[1]
+#     return dl_link.attrs['href']
+
+
+
+
+
 def subtitle_search (search):
-    data = {
-        "action": "ajaxsearchlite_search",
-        "aslp": f"{'+'.join(search.split())}",
-        "options": "qtranslate_lang=0&asl_gen[]=title&asl_gen[]=content&customset[]=post"
-    }
-    r = requests.post(url = "https://subkade.ir/wp-admin/admin-ajax.php", data = data)
-    return r.text
-
-def find_subtitle_links (text):
-    soup = bs(text, "html.parser")
-    tags = soup.find_all('a', attrs = {
-        "class": "asl_res_url"
-    })
-    links = []
-    for tag in tags:
-        links.append(tag.attrs['href'])
-    return links
-
-def get_subtitle_dl (url):
-    r = requests.get(url = url)
+    url = f"https://subkade.ir/?s={'+'.join(search.split())}&post_type=post"
+    r = requests.get(url)
     soup = bs(r.text, "html.parser")
-    res = soup.find_all("a", attrs = {
-        "class": "rounded-lg d-inline-block font-weight-bold sub_sgl_dl_btn"
-    })
-    dl_link = res[1]
-    return dl_link.attrs['href']
+    res = soup.find_all("div", attrs={"class": "cl_lg_2 cl_md_3 cl_sm_4 cl_6 a678"})
+    resDict = {}
+    for tag in res:
+        title = tag.find("h4", attrs = {'class': 'wrap'}).contents[0]
+        dl_link = tag.find("a")['href']
+        resDict[title] = dl_link
+    return resDict
+
+
+def get_subtitle_dl_links(url):
+    r = requests.get(url)
+    soup = bs(r.text, "html.parser")
+    res = soup.find_all("div", attrs = {'class': 'cl_sm_5'})
+    
+    resList = []
+    for sth in res:
+        res2 = sth.find("a")['href']
+        if len(re.findall("https:\/\/.*?\.subkade.ir\/.*", res2[:40])) != 0:
+            resList.append(res2)
+
+    return resList
+    
+
+def f2m_search(search):
+    url = f"https://www.f2m.site/?s={'+'.join(search.split())}&btnSubmit="
+    r = requests.get(url)
+    soup = bs(r.text, "html.parser")
+    res = soup.find_all("div", attrs={'id': 'post-title'})
+    resDict = {}
+    for sth in res:
+        try:
+            page_link = sth.find("h2").find("a")['href']
+            title = sth.find("h2").find("a").contents[0]
+            resDict[title] = page_link
+        except:
+            pass
+    return resDict
+
+
+def f2m_getDlLink(url):
+    pass
+
--- main.py
+++ main.py
@@ -2,34 +2,47 @@ import requests
 from bs4 import BeautifulSoup as bs
 from functions import *
 import re
+import sys
 
 def main():
-    search = input(">>")
-    links = movie_search(search = search)
-    number = int(input(">"))
-    res = movie_links(url = links[number-1])
-    dl_links = []
-    for sth in res:
-        dl_links.append(sth.attrs["href"])
+    if sys.argv[1].lower() == "movie":
+        search = input(">>")
+        linksDict = f2m_search(search)
+        
+        n = 0
+        for link in linksDict:
+            print(f"[{n+1}] {link}")
+            n += 1
+        
+        # links = movie_search(search = search)
+        # number = int(input(">"))
+        # res = movie_links(url = links[number-1])
+        # dl_links = []
+        # for sth in res:
+        #     dl_links.append(sth.attrs["href"])
 
-    print("-------------------------------------------")
-    for dl_link in dl_links:
-        print("\n")
-        print(dl_link)
-    print("\n-------------------------------------------")
-    raw_text = subtitle_search(search = search)
-    subtitle_links = find_subtitle_links(text = raw_text)
-    print("---------------------- subtitle ----------------------")
-    n = 0
-    for link in subtitle_links :
-        n += 1
-        print(f"[{n}]> {link}")
-        print("------------------")
-    sub_number = input(">")
-    subtitle_dl_link = get_subtitle_dl(subtitle_links[number-1])
-    print("-------------------------------------------")
-    print(subtitle_dl_link)
-    print("-------------------------------------------")
+        # print("-------------------------------------------")
+        # for dl_link in dl_links:
+        #     print("\n")
+        #     print(dl_link)
+        # print("\n-------------------------------------------")
+        print("---------------------- subtitle ----------------------")
+        subtitle_links = subtitle_search(search = search)
+        # subtitle_links = find_subtitle_links(text = raw_text)
+        n = 0
+        for link in subtitle_links:
+            print(f"[{n+1}] {link}")
+            n += 1
+
+        sub_number = input(">")
+        
+        subtitle_page_link = list(subtitle_links.values())[int(sub_number) - 1]
+        
+        print("-------------------------------------------")
+        for link in get_subtitle_dl_links(url = subtitle_page_link):
+            print(link)
+
+        print("-------------------------------------------")
 
 
 if __name__ == "__main__":
